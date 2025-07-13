f4002a85841bc8de51a3d50c3db2c554add17ae4
maricel <maricelmb@gmail.com>
2025-07-13 21:39:27 +1000

    Create the handler of product

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public  Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        public List<string> Category { get; set; } = new();
+        public Guid Description { get; set; } = default!;
+        public Guid ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, Guid Description, Guid ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Logic to create a product would go here
+            // For now, we return a dummy result
+            return Task.FromResult(new CreateProductResult(Guid.NewGuid()));
+        }
+    }
+    
+}
